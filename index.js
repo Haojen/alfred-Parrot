772430e3c65b6964714b963af481c117c183ad7b
haojen ma <ehazomm@gmail.com>
2017-06-26 14:31:27 +0800

    update

--- index.js
+++ index.js
@@ -9,15 +9,6 @@
 const alfy = require('alfy');
 const md5 = require('md5');
 
-const baiduTransAPI = {
-	url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
-	appid: '20170625000060222',
-	key: '2dlAYT1dFruhnrQj7yWL',
-	salt: 1039057373
-};
-
-const alfredQuery = alfy.input || 'yes to 日语';
-
 const LANGUAGE_MAP = {
 	zh:	'中文',
 	en:	'英语',
@@ -49,6 +40,15 @@ const LANGUAGE_MAP = {
 	vie: '越南语',
 };
 
+const baiduTransAPI = {
+	url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
+	appid: '20170625000060222',
+	key: '2dlAYT1dFruhnrQj7yWL',
+	salt: 1039057373
+};
+
+const alfredInputText = alfy.input || 'yes to en';
+
 function checkIsChineseText(str) {
 	return new RegExp("[\\u4E00-\\u9FFF]+", 'g').test(str);
 }
@@ -67,35 +67,43 @@ function checkUserDidCustomTargetLang(str) {
 }
 
 function getTransLanguage(str) {
-	// 判断用户是否手动转换了目标语言，如果是的话，使用用户的设定。
+	// 返回的数据格式
+	let alfredIO = {
+		queryText: str,
+		targetLanguage: 'zh'
+	};
+
+	// 如果用户指定了转换语言
 	let userCustomTargetLang = checkUserDidCustomTargetLang(str);
 	if (userCustomTargetLang) {
+		// 转换语言的标志是中文
 		if (checkIsChineseText(userCustomTargetLang)){
 			for (let langKey in LANGUAGE_MAP) {
 				LANGUAGE_MAP[langKey] === userCustomTargetLang ? userCustomTargetLang = langKey : '';
 			}
 		}
 
-		return userCustomTargetLang
+		alfredIO.queryText = str.split(' to ')[0];
+		alfredIO.targetLanguage = userCustomTargetLang;
+	}else if (checkIsChineseText(str)){
+		//默认如果用户输入的是中文的话，那直接转换成英文，如果用户输入的是非中文，则默认转换成中文。
+		alfredIO.targetLanguage = 'en'
 	}
 
-	//默认如果用户输入的是中文的话，那直接转换成英文，如果用户输入的是非中文，则默认转换成中文。
-	const isChinese = checkIsChineseText(str);
-	if (isChinese) {
-		return 'en'
-	}
-	return 'zh'
+	return alfredIO
 }
+
+const {queryText, targetLanguage} = getTransLanguage(alfredInputText);
 
 let params = {
 	method: 'POST',
 	body: {
-		q: alfredQuery,
+		q: queryText,
 		from: 'auto',
-		to: getTransLanguage(alfredQuery),
+		to: targetLanguage,
 		appid: baiduTransAPI.appid,
 		salt: baiduTransAPI.salt,
-		sign: md5(baiduTransAPI.appid + alfredQuery + baiduTransAPI.salt + baiduTransAPI.key)
+		sign: md5(baiduTransAPI.appid + alfredInputText + baiduTransAPI.salt + baiduTransAPI.key)
 	}
 };
 
@@ -109,4 +117,5 @@ alfy.fetch(baiduTransAPI.url, params).then(
 		});
 
 		alfy.output(result);
-	});
+	}
+);
