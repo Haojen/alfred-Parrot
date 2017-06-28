bc38c7970f709133fec52636151713b2fc42f6fc
haojen ma <ehazomm@gmail.com>
2017-06-28 23:56:53 +0800

    v0.1

--- index.js
+++ index.js
@@ -3,48 +3,57 @@ const alfy = require('alfy');
 
 const fetch = require('./config');
 const {parrot} = require('./parrot_core');
-const inputText = alfy.input || 'lol';
+const inputText = alfy.input || '你好 to jp';
 
 const {queryText, targetLanguage} = parrot.getTransLanguage(inputText);
 
 fetch.getTransitionResult(queryText, targetLanguage, 'youdao').then(res => {
-	// console.log(res.web, '返回的翻译结果', res)
-	// let result = res.trans_result.map(res => {
-	// 	return {
-	// 		title: res.dst,
-	// 		subtitle: res.src,
-	// 		arg: res.dst
-	// 	}
-	// });
-    //
+	let result;
 
+	// 如果有道无法进行翻译，则尝试调用百度接口
 	if (res.errorCode !== '0'){
-		//todo 错误处理
-		return
+		fetch.getTransitionResult(queryText, targetLanguage, 'baidu').then(res => {
+			result = res.trans_result.map(res => {
+				return {
+					title: res.dst,
+					subtitle: res.src,
+					arg: res.dst
+				}
+			});
+			alfy.output(result);
+		});
+	}else {
+		if (res.query && res.web) {
+			const web = res.web;
+			const query = res.query;
+			const basic = res.basic;
+			const explainsDetail = basic?basic.explains.join(''):'';
+			const explains = `; ${basic ? basic.phonetic : ''}`;
+
+			res.translation.push(`[${explainsDetail}]`)
+
+			web.unshift({
+				value: res.translation,
+				key: query + explains
+			});
+
+			result = web.map(trsItem => {
+				let title = trsItem.value.join();
+				return {
+					title,
+					subtitle: trsItem.key,
+					arg: trsItem.value[0]
+				}
+			})
+		}else {
+			result = res.translation.map(trsItem => {
+				return {
+					title: trsItem,
+					arg:trsItem
+				}
+			})
+		}
+		alfy.output(result);
 	}
 
-	const web = res.web;
-	const query = res.query;
-	const explains = res.basic.explains.reduce((a, b) => a+','+b);
-	const basic = `解释：${explains}, 拼读：${res.basic.phonetic||'无'}`
-
-	res.translation.push(basic)
-
-	web.unshift({
-		value: res.translation,
-		key: query
-	});
-
-	let result = web.map(trsItem => {
-		let title = trsItem.value.reduce((a,b) => a+','+b)
-		return {
-			title,
-			subtitle: trsItem.key,
-			arg: trsItem.value[0]
-		}
-	})
-
-	// console.log(result)
-    //
-    alfy.output(result);
-})
+});
