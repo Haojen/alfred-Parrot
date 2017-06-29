eb2d4bdf4045228eb2bca27b44f15f22ff34b3d6
haojen ma <ehazomm@gmail.com>
2017-06-29 20:53:18 +0800

    update

--- index.js
+++ index.js
@@ -17,69 +17,58 @@ const {queryText, targetLanguage} = parrot.getTransLanguage(inputText);
 fetch.getTransitionResult(queryText, targetLanguage, 'youdao').then(res => {
 	let result;
 
-	// 如果有道无法进行翻译，则尝试调用百度接口
-	if (res.errorCode !== '0'){
-		fetch.getTransitionResult(queryText, targetLanguage, 'baidu').then(res => {
-			result = [{
-				title: '查询中..',
-				subtitle: '请稍等'
-			}];
+	if (res.query && res.web) {
+		const web = res.web;
+		const query = res.query;
+		const basic = res.basic;
+		const explainsDetail = basic?basic.explains.join(''):'';
+		const explains = `; ${basic ? basic.phonetic : ''}`;
 
-			if (res.error_code !== '52000') {
-				return
-			}
+		res.translation.push(`[${explainsDetail}]`);
 
-			result = res.trans_result.map(res => {
-				return {
-					title: res.dst,
-					subtitle: res.src,
-					arg: res.dst,
-				}
-			});
-
-			if (userWantPlaySound) {
-				parrot.playSound(res.dst);
-			}
-
-			alfy.output(result);
+		web.unshift({
+			value: res.translation,
+			key: query + explains
 		});
+
+		result = web.map(trsItem => {
+			let title = trsItem.value.join();
+			return {
+				title,
+				subtitle: trsItem.key,
+				arg: trsItem.value[0],
+			}
+		})
 	}else {
-		if (res.query && res.web) {
-			const web = res.web;
-			const query = res.query;
-			const basic = res.basic;
-			const explainsDetail = basic?basic.explains.join(''):'';
-			const explains = `; ${basic ? basic.phonetic : ''}`;
-
-			res.translation.push(`[${explainsDetail}]`);
-
-			web.unshift({
-				value: res.translation,
-				key: query + explains
-			});
-
-			result = web.map(trsItem => {
-				let title = trsItem.value.join();
-				return {
-					title,
-					subtitle: trsItem.key,
-					arg: trsItem.value[0],
-				}
-			})
-		}else {
-			result = res.translation.map(trsItem => {
-				return {
-					title: trsItem,
-					arg:trsItem,
-				}
-			})
-		}
-
-		if (userWantPlaySound) {
-			parrot.playSound(res.translation[0]);
-		}
-
-		alfy.output(result);
+		// 非中译英，英译中的结果
+		result = res.translation.map(trsItem => {
+			return {
+				title: trsItem,
+				arg:trsItem,
+			}
+		})
 	}
 
+	alfy.output(result);
+
+	// 播放声音
+	userWantPlaySound ? parrot.playSound(res.translation[0]):null;
+}, () => {
+	fetch.getTransitionResult(queryText, targetLanguage, 'baidu').then(res => {
+		let result = res.trans_result.map(res => {
+			return {
+				title: res.dst,
+				subtitle: res.src,
+				arg: res.dst,
+			}
+		});
+
+		alfy.output(result);
+		userWantPlaySound ? parrot.playSound(res.dst):null;
+	}, err => {
+		alfy.output([{
+			title: err,
+			arg: err
+		}]);
+	});
 });
--- script/config.js
+++ script/config.js
@@ -1,6 +1,7 @@
 const md5 = require('md5');
 const alfy = require('alfy');
 const {language_map, yd_language} = require('./language_map');
+const errorMap = require('./error_map');
 
 const config = {
 	youdao: {
@@ -37,7 +38,15 @@ function getTransitionResult(query, targetLanguage, type = 'youdao') {
 		sign: md5(appid + query + salt + key)
 	};
 
-	return alfy.fetch(url,{body: Object.assign(defaultParams, params)})
+	return alfy.fetch(url,{
+		body: Object.assign(defaultParams, params)
+	}).then( res => {
+		return new Promise((success, fail) => {
+			errorMap[res.errorCode||res.error_code] ?
+			fail(errorMap[res.errorCode||res.error_code]):
+			success(res);
+		});
+	})
 }
 
 exports.getTransitionResult = getTransitionResult;
