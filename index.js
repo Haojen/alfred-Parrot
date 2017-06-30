253ac9564b2c90c6e7b4d8d39ed27d6753324d0e
haojen ma <ehazomm@gmail.com>
2017-06-30 11:54:53 +0800

    refactor code

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const alfy = require('alfy');
-
 const {parrot} = require('./script/parrot_core');
-const inputText = alfy.input ? alfy.input.trim() : ':debug';
 
-parrot.getTransLanguage(inputText, result => alfy.output(result)
-);
+if (process.env.NODE_ENV === 'development') {
+	const textInput = ':Debug';
+	parrot.getTransLanguage(textInput, result => console.log(result));
+
+	return;
+}
+
+parrot.getTransLanguage(alfy.input.trim(), result => alfy.output(result));
