5b2511b05233358845b9a9188b8aca70b5dfb7a7
haojen ma <ehazomm@gmail.com>
2017-07-01 08:38:28 +0800

    add var workflow fearture

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const alfy = require('alfy');
 const {parrot} = require('./script/parrot_core');
-
+const alfredNotifier = require('alfred-notifier');
 if (process.env.NODE_ENV === 'development') {
 	const textInput = ':Debug';
 	parrot.getTransLanguage(textInput, result => console.log(result));
 
 	return;
 }
+
+alfredNotifier();
 
 parrot.getTransLanguage(alfy.input.trim(), result => alfy.output(result));
--- script/config.js
+++ script/config.js
@@ -1,14 +1,37 @@
+const env = process.env;
+/*
+baidu
+* appid:'20170625000060222'
+* key: '2dlAYT1dFruhnrQj7yWL'
+* */
+let user_yd = {
+	key: '',
+	appid: ''
+};
+
+let user_bd = {
+	key: '',
+	appid: ''
+};
+
+if (env.config_youdao) {
+	user_yd = JSON.parse(env.config_youdao);
+}
+if (env.config_baidu) {
+	user_bd = JSON.parse(env.config_baidu);
+}
+
 const config = {
 	youdao: {
 		url: 'https://openapi.youdao.com/api',
-		appid: '0d68776be7e9be0b',
-		key: 'MIbu7DGsOPdbatL9KmgycGx0qDOzQWCM',
+		appid: user_yd.appid,
+		key: user_yd.key,
 		salt: 1039057373
 	},
 	baidu: {
 		url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
-		appid: '20170625000060222',
-		key: '2dlAYT1dFruhnrQj7yWL',
+		appid: user_bd.appid,
+		key: user_bd.key,
 		salt: 1039057373
 	}
 };
